965bc72a90a4d9ba69c5569e61275a3a4a8b87d9
ryansweigart3 <69874256+ryansweigart3@users.noreply.github.com>
2023-01-21 12:19:58 -0500

    Updates epl_scrape.py

--- epl_scrape.py
+++ epl_scrape.py
@@ -1,8 +1,21 @@
-import itertools
+from datetime import date
+from typing import final
 from gazpacho import get, Soup
 import pandas as pd
+import itertools
+import psycopg2
 
+# initialize date
+datetime = date.today()
+timestamp = datetime.strftime("%d%m%y")
 
+# initialize database
+conn = psycopg2.connect(
+    database='test_db',
+    user='****',
+    password='**********',
+    host='0.0.0.0'
+)
 
 # Initialize URL and parse html
 url = "https://scores.nbcsports.com/epl/standings.asp"
@@ -12,17 +25,22 @@ soup = Soup(html)
 div = soup.find('div', {'id': 'shsIFBStandings'})
 
 #Initialize dataframe columns used later
-cols = ['Team',
-        'Games Played',
-        'Wins',
-        'Draws',
-        'Losses',
-        'Goals Forced',
-        'Goals Allowed',
-        'Goal Difference',
-        'Points']
+cols = [
+        'team',
+        'games_played',
+        'wins',
+        'draws',
+        'losses',
+        'goals_forced',
+        'goals_allowed',
+        'goal_difference',
+        'points']
 
+# Function scrapes NBC sports to gather
+# current EPL table and imports into pgsql db
 def main():
+    print('*** Beginning web scrape of NBC Sports to get most up-to-date EPL Standings ***')
+
     standing = div.find('td', {'class': 'shsNumD'})
     standingsList = []
     #print(standingEven)
@@ -33,7 +51,8 @@ def main():
     # Cleaning up standingsList
     k = 10
 
-    standingsList = standingsList[k:]
+    standingsList = standingsList[k: ]
+    # print(standingsList)
 
     gamesPlayed = standingsList[0::10]
     wins = standingsList[1::10]
@@ -51,32 +70,45 @@ def main():
         evenResult = i.text
         teamEvenList.append(evenResult)
 
-    # print(teamEvenList)
-
     teamOdd = div.find('tr', {'class': 'shsRow1Row'})
     teamOddList = []
     for i in teamOdd:
         oddResult = i.text
         teamOddList.append(oddResult)
 
-    # print(teamOddList)
-
     # Merge two lists while keeping original indices
     finalStandings = [item for sublist in zip(teamEvenList, teamOddList) for item in sublist]
 
-    # print(finalStandings)
-
     df = pd.DataFrame(list(zip(finalStandings, gamesPlayed, wins, draws, losses, goalsForced, goalsAllowed, goalDifference, points)), 
-                     columns = cols)
+                    columns = cols)
 
-    df = df.set_index('Team')
-    print(df)
+   
+    # reset index    
+    df = df.set_index('team')
+    print(df.dtypes)
     output = df.to_csv()
+    
 
-    with open ('eplFullStandings.csv', 'w+') as of:
+    with open ('eplFullStandings' + timestamp + '.csv', 'w+') as of:
         of.write(output)
-    print("Complete!")
+        of.close()
+    
+    cur = conn.cursor()
 
+    # create table
+    cur.execute('CREATE TABLE standings (team char(100), games_played char(100), wins char(100), draws char(100), losses char(100), goals_forced char(100), goals_allowed char(100), goal_difference char(100), points char(100));')
+
+    # add df to database
+    with open('eplFullStandings' + timestamp + '.csv', 'r') as f:    
+        next(f) # Skip the header row.
+        cur.copy_from(f, 'standings', sep=',')
+    
+    conn.commit()
+    conn.close()
+    cur.close()
+    
+    print("That's it! Scrape is complete!")
+    
 
 if __name__ == "__main__":
     main()
